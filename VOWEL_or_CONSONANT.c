316c558ae99987dc5836b9f9201cfc00090074af
23MHCQ05099 <147489556+23MHCQ05099@users.noreply.github.com>
2023-11-01 18:40:58 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u')     {         printf("VOWEL ");     }     else if (ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U')     {         printf("VOWEL ");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u')
+    {
+        printf("VOWEL
+");
+    }
+    else if (ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U')
+    {
+        printf("VOWEL
+");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
